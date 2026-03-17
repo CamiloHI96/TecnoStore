3eef77dd590f0ba5bebc8705e4963e42b0068db0
Camilo Hernandez <camilohi.1196@gmail.com>
2026-03-16 22:04:41 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return redirect()->route('products.index');
-});
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
-Route::resource('products', ProductController::class);
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
